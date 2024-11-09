5263f4370bcf1ee7d69fefecea9bf814087fc49b
hamidreza <hamedreza1992@gmail.com>
2024-11-09 15:56:25 +0330

    create soignal in apps core

---
+++ apps/account/signals.py
@@ -0,0 +1,13 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from apps.account.models import User
+from service.email import EmailService
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        print(type(instance.email))
+        email = EmailService(subject='welcome',
+                             template_name='mail/welcome.html',
+                             to_email=instance.email)
+        email.send()
