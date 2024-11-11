8cf6b514293c54d430416d6ee982d17dfad6808a
hamidreza <hamedreza1992@gmail.com>
2024-11-11 22:02:26 +0330

    create midel abstact logical delete

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class LogicalDelete(models.Model):
+    is_deleted = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
