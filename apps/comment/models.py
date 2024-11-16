ed758deee7a3804c818958d9c21d1bc42046cac5
hamidreza <hamedreza1992@gmail.com>
2024-11-16 19:04:57 +0330

    update comment model

--- apps/comment/models.py
+++ apps/comment/models.py
@@ -1,17 +1,21 @@
 from django.db import models
-from apps.core.models import TimeCreateMixin, LogicalDeleteMixin
-from config import settings
+from apps.core.models.logicaldelete import LogicalDeleteMixin
+from apps.core.models.timelogical import TimeCreateMixin
+
 from apps.advertising.models import Advertising
-
-
+from apps.core.managers import BasicLogicalDeleteManager
+from django.contrib.auth import get_user_model
+User = get_user_model()
 # Create your models here.
 
 class comment(TimeCreateMixin, LogicalDeleteMixin):
     massage = models.TextField(max_length=400, null=True, blank=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    to_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='user_comments',related_query_name='user_comments')
+    to_user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='to_user_comments',related_query_name='to_user_comments')
     advertised = models.ForeignKey(Advertising, on_delete=models.CASCADE)
     expires_at = None
+
+    object = BasicLogicalDeleteManager()
 
     def __str__(self):
         return f'{self.user} to {self.to_user}'
