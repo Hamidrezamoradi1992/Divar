45321a1ab8332d1d34449d12666a295b01b0447c
hamidreza <hamedreza1992@gmail.com>
2024-11-18 12:24:44 +0330

    set signal and config signal in apps advertise

--- apps/advertising/admin.py
+++ apps/advertising/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from apps.advertising.models import Category, FieldCategory
+from apps.advertising.models import Category, FieldCategory,Advertising
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(FieldCategory)
+admin.site.register(Advertising)
+
