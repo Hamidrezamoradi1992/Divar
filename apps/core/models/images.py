4c6c5314a17e5a0607221fedefe92d793afce0ee
hamidreza <hamedreza1992@gmail.com>
2024-11-20 12:24:54 +0330

    update model in (def clean) and set validator new its neme(dynamic_upload_path (created chatgpt))

--- apps/core/models/images.py
+++ apps/core/models/images.py
@@ -16,30 +16,33 @@ class Image(LogicalDeleteMixin, TimeCreateMixin):
     name = models.CharField(max_length=100)
     content_type = models.ForeignKey(ContentType,
                                      on_delete=models.CASCADE)
-    image = models.ImageField(upload_to=f'{content_type}product_images/',
-                              validators=[CustomValidators.image_validator])
-    order_id = models.PositiveSmallIntegerField()
+    image = models.ImageField(upload_to=f'Image_images/',
+                              null=True,
+                              blank=True)
+    instance_id = models.PositiveSmallIntegerField()
     alt = models.TextField(blank=True,
                            null=True)
     is_cover = models.BooleanField(default=False)
 
     generics = GenericForeignKey('content_type',
-                                 'order_id')
+                                 'instance_id')
 
-    def clean(self):
+    def clean(self, **kwargs):
         if not (self.content_type.model in ('category', 'advertising', 'user')):
             raise ValidationError('class not valid')
         if self.is_cover:
             if self.content_type == 'advertising':
                 cover_images = Image.objects.filter(content_type=self.content_type,
-                                                    order_id=self.order_id)
+                                                    instance_id=self.instance_id)
                 if cover_images.is_:
                     raise ValidationError('Each advertising can only have one cover image.')
+        if self.image:
+            CustomValidators.image_validator(self.image)
         if self.content_type.model in ('category', 'user'):
             validate_image_on_ripit = Image.objects.filter(content_type=self.content_type,
-                                                           order_id=self.order_id).exists()
-            if validate_image_on_ripit:
-                raise ValidationError('Each no user can only have one cover image.')
+                                                           instance_id=self.instance_id).exists()
+            # if validate_image_on_ripit:
+            #     raise ValidationError('Each no user can only have one cover image.')
 
     def save(self, *args, **kwargs):
         self.clean()
--- apps/core/validators.py
+++ apps/core/validators.py
@@ -1,5 +1,6 @@
 import re
-
+import os
+from django.utils.text import slugify
 from rest_framework.exceptions import ValidationError
 
 
@@ -15,3 +16,17 @@ class CustomValidators:
     @classmethod
     def password_validator(cls, password):
         return re.match(cls._PASSWORD_PATTERN, password)
+
+    @classmethod
+    def dynamic_upload_path(instance, filename):
+        # پیدا کردن مدل مرتبط با content_type
+        model_name = instance.content_type.model
+        # تولید پوشه بر اساس مدل
+        base_folder = f"{model_name}_images"
+        # استخراج پسوند فایل
+        extension = os.path.splitext(filename)[1]
+        # نام فایل با استفاده از slugify
+        name = slugify(instance.name) if instance.name else "default"
+        # تولید مسیر نهایی فایل
+        new_filename = f"{name}_{instance.instance_id}{extension}"
+        return os.path.join(base_folder, new_filename)
