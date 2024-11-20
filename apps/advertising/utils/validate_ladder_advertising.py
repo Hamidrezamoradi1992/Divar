c5fb3b9b5c6bb9ffb4b1eb849a2d3c0c6b190404
hamidreza <hamedreza1992@gmail.com>
2024-11-20 17:53:25 +0330

    update validator in category

--- apps/advertising/utils/validate_ladder_advertising.py
+++ apps/advertising/utils/validate_ladder_advertising.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 import random
+from typing import List
 
 from django.utils import timezone
 
@@ -7,21 +8,22 @@ from apps.advertising.models import Advertising
 
 
 class ValidateLadderAdvertising:
-    __slots__ = ('queryset', 'create_at', 'delete_at', 'ladder_count')
+    __slots__ = ('queryset', 'create_at', 'delete_at', 'ladder_count', 'category_id')
 
     _LADDER_ADVERTISING_MODEL = {}
     _LADDER_INSTANCE = []
 
     def __new__(cls, queryset, *args, **kwargs):
         if queryset.id not in cls._LADDER_ADVERTISING_MODEL:
-            instance = super().__new__(cls)  # یک نمونه جدید ایجاد می‌کند
+            instance = super().__new__(cls)
             cls._LADDER_ADVERTISING_MODEL[queryset.id] = instance
-            return instance  # برگرداندن نمونه جدید
+            return instance
         return cls._LADDER_ADVERTISING_MODEL[queryset.id]
 
-    def __init__(self, queryset):
-        if not hasattr(self, 'queryset'):  # چک می‌کند که آیا قبلاً مقداردهی شده است یا نه
+    def __init__(self, queryset, category):
+        if not hasattr(self, 'queryset'):
             self.queryset = queryset
+            self.category_id = category
             self.create_at = timezone.now()
             self.delete_at = timezone.now() + timedelta(hours=24)
             self.ladder_count = 1
@@ -37,14 +39,16 @@ class ValidateLadderAdvertising:
             return ValidateLadderAdvertising._LADDER_INSTANCE.copy()
 
     @staticmethod
-    def get_ladder_advertising():
+    def get_ladder_advertising(categories_id:list=None):
         if ValidateLadderAdvertising._LADDER_INSTANCE:
             random_items = ValidateLadderAdvertising._random_item()
+            if categories_id:
+                random_items = [i for i in random_items if i.category_id in categories_id]
             for item in random_items:
                 if item.ladder_count > 3:
                     random_items.remove(item)
-                    advertise=Advertising.objects.get(id=item.queryset.id)
-                    advertise.ladder=False
+                    advertise = Advertising.objects.get(id=item.queryset.id)
+                    advertise.ladder = False
                     advertise.save()
                     ValidateLadderAdvertising._LADDER_INSTANCE.remove(item)
                     ValidateLadderAdvertising._LADDER_ADVERTISING_MODEL.pop(item.queryset.id)
