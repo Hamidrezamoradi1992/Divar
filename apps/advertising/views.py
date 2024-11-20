5b2d34b27137c0faeb4c20b0662563b77ff32846
hamidreza <hamedreza1992@gmail.com>
2024-11-20 17:58:40 +0330

    start create view

--- apps/advertising/views.py
+++ apps/advertising/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework import status
+from apps.advertising.models import Advertising,SaveValueField,Category,FieldCategory,City,State
 # Create your views here.
+class AllAdvertisingView(ListAPIView):
+    """
+        - view all advertising
+
+        {
+
+
+
+        }
+
+    """
+    queryset = Advertising.objects.all().order_by('-created_at')
+    serializer_class = []
+    permission_classes = []
+
