630b195667f7f162405db151e9c46af63de56c78
hamidreza <hamedreza1992@gmail.com>
2024-11-21 18:20:37 +0330

    set new url in view for view all fields

--- apps/advertising/urls.py
+++ apps/advertising/urls.py
@@ -5,4 +5,9 @@ from apps.advertising import views
 urlpatterns = [
 
     path('', views.AllAdvertisingView.as_view(), name='advertising-list'),
+    path('api/view/advertise/cartegory/<int:category_id>', views.ViewAdvertisingForCategory.as_view()),
+
+    #add advertise
+    path('api/add/advertise/<int:category_id>', views.AddAdvertiseView.as_view()),
+    path('api/add/advertise/all_category', views.ViewAllCategory.as_view()),
 ]
