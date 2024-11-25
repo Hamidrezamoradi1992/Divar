7864347927a9f93f7adb19d7cad13c1b14464062
hamidreza <hamedreza1992@gmail.com>
2024-11-25 20:04:20 +0330

    admin panel published template and view

--- storage/static/js/adminpanel/UserAdminPanel/GetContextUser.js
+++ storage/static/js/adminpanel/UserAdminPanel/GetContextUser.js
@@ -2,17 +2,23 @@ function get_user(user_id) {
     const data = fetchWithAuth(`http://localhost:${domainPort}/accounts/api/user/${user_id}`, {
         method: 'GET',
     })
-    let formContextEl = document.querySelector('#contextDateUser')
+
     data.then(response => {
-        console.log(response)
-        console.log(`${response.image_Official_photo && response.image_Official_photo.length > 0 ? response.image_Official_photo.url :" "}`)
-        formContextEl.innerHTML = `
+            console.log(response)
+            console.log(`${response.image_Official_photo && response.image_Official_photo.length > 0 ? response.image_Official_photo.url : " "}`)
+            formContextEl.innerHTML = `
                 <div class=" avatar my-5">
-                    <div class="skeleton mask mask-hexagon w-52">
-                        <img src="${response.image_Official_photo}"
+                   <div class="skeleton mask mask-hexagon w-52">
+                       <img src="${response.image_Official_photo}"
                                         alt="imageUser"  class="max-w-[95vw] max-h-[95vh] "/>
                     </div>
+                 </div>
+                    
                 </div>
+
+                   
+                  
+          
             <div>
                 <h1 class="font-sans  italic capitalize m-auto ">first name : ${response.first_name}</h1>
                 <h1 class="font-sans  italic capitalize m-auto ">last name : ${response.last_name}</h1>
