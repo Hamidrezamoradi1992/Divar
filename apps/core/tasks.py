398866ea02181a8fcd7f04744d4a1eea0fa7028e
hamidreza <hamedreza1992@gmail.com>
2024-12-18 09:42:12 +0330

    new congfig core task and new

--- apps/core/tasks.py
+++ apps/core/tasks.py
@@ -1,18 +1,28 @@
 from time import sleep
 from celery import shared_task
-
-from service.email import EmailService
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 @shared_task
-def send_email(subject, template_name, to_email, context):
-    email = EmailService(
-        subject=subject,
-        template_name=template_name,
-        to_email=to_email,
-        context=context
-    )
-    print(f"SEND{context} {to_email}")
-    email.send()
-    print(f"COMPILED {context} {to_email}")
+def send_email(subject, to_email, context,template_name=None):
+        subject = subject
+        message = context
+        recipient_list = to_email
+        print(f"SEND{context} {to_email}")
+        send_mail(subject, message, None, recipient_list)
+        print(f"COMPILED {context} {to_email}")
 
+
+
+    # print(f"SEND{context} {to_email}")
+    # send_mail(subject=subject,message=context,from_email=settings.DEFAULT_FROM_EMAIL,recipient_list=to_email)
+    # print(f"COMPILED {context} {to_email}")
+
+# def send_simple_email():
+# subject = "Test Email from Django"
+# message = "This is a test email sent from a Django project."
+# recipient_list = ["recipient@example.com"]
+#
+# try:
+# send_mail(subject, message, None, recipient_list)
